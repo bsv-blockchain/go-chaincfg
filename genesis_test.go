6114f544b144201a91c7ca114d0617b5540dd3ad
Dylan Murray <dylan@britevue.com>
2025-08-19 11:26:13 -0400

    Fix lint

--- genesis_test.go
+++ genesis_test.go
@@ -331,7 +331,7 @@ var teraTestNetGenesisBlockBytes = []byte{
 	0x19, 0x67, 0xf1, 0xa6, 0x71, 0x30, 0xb7, 0x10, 0x5c, 0xd6, 0xa8, 0x28, 0xe0, 0x39, 0x09, 0xa6, /* |.g..q0..\..(.9..| */
 	0x79, 0x62, 0xe0, 0xea, 0x1f, 0x61, 0xde, 0xb6, 0x49, 0xf6, 0xbc, 0x3f, 0x4c, 0xef, 0x38, 0xc4, /* |yb...a..I..?L.8.| */
 	0xf3, 0x55, 0x04, 0xe5, 0x1e, 0xc1, 0x12, 0xde, 0x5c, 0x38, 0x4d, 0xf7, 0xba, 0x0b, 0x8d, 0x57, /* |.U......\8M....W| */
-	0x8a, 0x4c, 0x70, 0x2b, 0x6b, 0xf1, 0x1d, 0x5f, 0xac, 0x00, 0x00, 0x00, 0x00,                   /* |.Lp+k.._.....   | */
+	0x8a, 0x4c, 0x70, 0x2b, 0x6b, 0xf1, 0x1d, 0x5f, 0xac, 0x00, 0x00, 0x00, 0x00, /* |.Lp+k.._.....   | */
 }
 
 // TestTeraTestNetGenesisProofOfWork tests that the tera test network genesis
@@ -339,28 +339,28 @@ var teraTestNetGenesisBlockBytes = []byte{
 func TestTeraTestNetGenesisProofOfWork(t *testing.T) {
 	// Get the genesis block
 	genesisBlock := TeraTestNetParams.GenesisBlock
-	
+
 	// Calculate the hash
 	hash := genesisBlock.BlockHash()
-	
+
 	// Get the target from bits
 	bits := genesisBlock.Header.Bits
 	mantissa := bits & 0x00ffffff
 	exponent := bits >> 24
-	
+
 	target := new(big.Int).SetInt64(int64(mantissa))
 	if exponent > 3 {
 		target.Lsh(target, 8*(uint(exponent)-3))
 	}
-	
+
 	// Check if hash meets difficulty
 	hashInt := new(big.Int).SetBytes(hash.CloneBytes())
-	
+
 	if hashInt.Cmp(target) > 0 {
 		t.Fatalf("TestTeraTestNetGenesisProofOfWork: Genesis block does not meet difficulty requirement. "+
 			"Hash: %s, Target: %064x", hash, target.Bytes())
 	}
-	
+
 	// Verify the nonce is the expected value
 	expectedNonce := uint32(0x00000002)
 	if genesisBlock.Header.Nonce != expectedNonce {
